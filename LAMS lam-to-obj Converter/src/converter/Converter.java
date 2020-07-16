3702737296fceea9652b35e95b8b50b3b9c1429c
tjensen123 <31261947+tjensen123@users.noreply.github.com>
2020-07-16 10:36:01 -0600

    added status label

--- LAMS lam-to-obj Converter/src/converter/Converter.java
+++ LAMS lam-to-obj Converter/src/converter/Converter.java
@@ -50,6 +50,8 @@ public class Converter extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        statusLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Find");
@@ -79,15 +81,23 @@ public class Converter extends javax.swing.JFrame {
             }
         });
 
+        jLabel2.setText("Status:");
+
+        statusLabel.setText("...");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 280, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 274, Short.MAX_VALUE)
+                    .addComponent(statusLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -104,11 +114,16 @@ public class Converter extends javax.swing.JFrame {
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jLabel2)
+                    .addComponent(statusLabel))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton3)
                 .addContainerGap())
         );
+
+        statusLabel.getAccessibleContext().setAccessibleName("StatusLabel");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -116,7 +131,12 @@ public class Converter extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         if(!lamFile.equals(null)){
             converter = new LamToObjConverter();
-            converter.convert(lamFile);
+            if(converter.convert(lamFile)){
+                statusLabel.setText("Conversion Successful!");
+            }
+            else{
+                statusLabel.setText("Conversion Failed!");
+            }
         }
     }//GEN-LAST:event_jButton2ActionPerformed
 
@@ -125,6 +145,7 @@ public class Converter extends javax.swing.JFrame {
         if(res == JFileChooser.APPROVE_OPTION){
             this.lamFile = chooser.getSelectedFile();
             jTextField1.setText(chooser.getName(lamFile));
+            statusLabel.setText(".lam File Selected.");
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -136,7 +157,9 @@ public class Converter extends javax.swing.JFrame {
                 try {                
                     this.objFile.createNewFile();
                     converter.createObjFile(this.objFile);
+                    statusLabel.setText(".obj File Saved");
                 } catch (IOException ex) {
+                    statusLabel.setText(".obj File Save Failed.");
                     Logger.getLogger(Converter.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 
@@ -185,7 +208,9 @@ public class Converter extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel statusLabel;
     // End of variables declaration//GEN-END:variables
     
 }
